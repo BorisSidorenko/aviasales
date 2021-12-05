d17c5a4834871a88d8439079801b4819ea76d4b9
Boris <boris.m.sidorenko@gmail.com>
2021-12-05 16:37:40 +0300

    added component ShowMoreButton

---
+++ src/components/show-more-button/ShowMoreButton.js
@@ -0,0 +1,16 @@
+import "./ShowMoreButton.css";
+import { DISPLAY_TICKETS_BY_DEFAULT } from "../../utils/constants";
+
+export default function ShowMoreButton({ changeDisplayTicketsAmount }) {    
+    const handleButtonClick = (() => {
+        return function () {
+            changeDisplayTicketsAmount((prev) => prev + DISPLAY_TICKETS_BY_DEFAULT);                             
+        }
+    })();
+
+    return (
+        <button className="btn-more" onClick={handleButtonClick}>
+            {`показать ещё ${DISPLAY_TICKETS_BY_DEFAULT} билетов`}
+        </button>
+    )
+}
