c29427771c46e13aae7d6ce1b9cc089412746b92
Boris <boris.m.sidorenko@gmail.com>
2021-12-05 16:39:22 +0300

    modified TicketList component to display Tickets

--- src/components/ticket-list/TicketList.js
+++ src/components/ticket-list/TicketList.js
@@ -1,10 +1,16 @@
 import Ticket from "../ticket/Ticket";
 import "./TicketList.css";
 
-export default function TicketList() {
+export default function TicketList({ tickets, amountTodisplay }) {       
+    const ticketsToDisplay = tickets.slice(0, amountTodisplay);
+
     return (
         <ul className="ticket-list">
-            <Ticket />            
+            {ticketsToDisplay.length > 0 && ticketsToDisplay.map((ticket) => (
+                <li key={`${ticket.carrier}-${ticket.price}`} className="ticket-item">                                    
+                    <Ticket ticket={ticket}/>
+                </li>
+            ))}
         </ul>
     )
 }
