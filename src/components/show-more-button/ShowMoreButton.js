e3adbeef6018e5620ae820c40945d4454b2c911b
Boris <boris.m.sidorenko@gmail.com>
2021-12-06 21:13:14 +0300

    added some features to ticket component. WIP

--- src/components/show-more-button/ShowMoreButton.js
+++ src/components/show-more-button/ShowMoreButton.js
@@ -2,11 +2,7 @@ import "./ShowMoreButton.css";
 import { DISPLAY_TICKETS_BY_DEFAULT } from "../../utils/constants";
 
 export default function ShowMoreButton({ changeDisplayTicketsAmount }) {    
-    const handleButtonClick = (() => {
-        return function () {
-            changeDisplayTicketsAmount((prev) => prev + DISPLAY_TICKETS_BY_DEFAULT);                             
-        }
-    })();
+    const handleButtonClick = () => changeDisplayTicketsAmount((prev) => prev + DISPLAY_TICKETS_BY_DEFAULT);
 
     return (
         <button className="btn-more" onClick={handleButtonClick}>
--- src/components/ticket/Ticket.js
+++ src/components/ticket/Ticket.js
@@ -1,36 +1,48 @@
 import "./Ticket.css";
 
-export default function Ticket() {
+export default function Ticket({ ticket }) {   
+    const { price, carrier, segments} = ticket; 
+
+    const getStopsHeader = ({ stops }) => {        
+        switch (stops.length) {
+            case 0:
+                return "прямой";
+            case 1:
+                return `${stops.length} пересадка`;
+            case 2: 
+            case 3: 
+            case 4:
+                return `${stops.length} пересадки`;
+            case 5:
+                return `${stops.length} пересадок`;
+            default:
+                return `${stops.length} пересадок`;
+        }
+    }
+    const getStopsFromSegment = ({ stops }) => stops.length > 0 ? stops.join(", ") : "—";
+
+    const getPrice = (price) => price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+
     return (
         <article className="ticket">
             <div className="ticket-header">
-                <p className="price">13 400 Р</p>
-                <img src="https://pics.avs.io/110/36/S7.png" className="company-logo" alt="Логотип авиакомпании"/>
+                <p className="price">{`${getPrice(price)} Р`}</p>
+                <img src={`https://pics.avs.io/110/36/${carrier}.png`} className="company-logo" alt={`Логотип авиакомпании ${carrier}`}/>
             </div>
-            <div className="ticket-details">
-                <div className="details-header">
-                    <p className="route"><span>MOW</span> - <span>HKT</span></p>
-                    <p className="duration"><span>в пути</span></p>
-                    <p className="stops"><span>1 пересадка</span></p>
-                </div>
-                <div className="details-info">
-                    <p className="route"><span>10:45</span> – <span>08:00</span></p>
-                    <p className="duration"><span>21ч 15м</span></p>
-                    <p className="stops"><span>HKG</span></p>
-                </div>                
-            </div>
-            <div className="ticket-details">
-                <div className="details-header">
-                    <p className="route"><span>MOW</span> - <span>HKT</span></p>
-                    <p className="duration"><span>в пути</span></p>
-                    <p className="stops"><span>1 пересадка</span></p>
-                </div>
-                <div className="details-info">
-                    <p className="route"><span>10:45</span> - <span>08:00</span></p>
-                    <p className="duration"><span>21ч 15м</span></p>
-                    <p className="stops"><span>HKG</span></p>
-                </div>
-            </div>
+            {segments.map((segment) => (
+                <div className="ticket-details" key={`${segment.origin}${price}${carrier}`}>
+                    <div className="details-header">
+                        <p className="route"><span>{segment.origin}</span> - <span>{segment.destination}</span></p>
+                        <p className="duration"><span>в пути</span></p>
+                        <p className="stops"><span>{getStopsHeader(segment)}</span></p>
+                    </div>
+                    <div className="details-info">
+                        <p className="route"><span>10:45</span> – <span>08:00</span></p>
+                        <p className="duration"><span>{segment.duration}</span></p>
+                        <p className="stops"><span className={segment.stops.length > 0 ? "" : "no-stops"}>{getStopsFromSegment(segment)}</span></p>
+                    </div>                
+                </div>     
+            ))}       
         </article>
     )
 }
