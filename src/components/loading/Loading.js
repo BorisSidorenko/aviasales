224d1bab17b63fbc143f19f9cdd29223ac444d7a
Boris <boris.m.sidorenko@gmail.com>
2021-12-12 15:46:11 +0300

    added loading screen

---
+++ src/components/loading/Loading.js
@@ -0,0 +1,9 @@
+import "./Loading.css";
+
+export default function Loading() {
+    return (
+        <div className="wrapper-loader">
+            <div className="loader">Loading...</div>
+        </div>                
+    )
+}
